85188c0ccd69433dc16e41c696eaedc4c7ab24b4
kloughead <kvnloughead@gmail.com>
2019-06-02 20:33:21 +0000

    Committing this thing before I break it again.

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,40 @@
+from requests.exceptions import HTTPError
 import requests
+import sqlite3
+from pathlib import Path
+
+
+COLUMNS = ['github_id', 'html_url', 'git_pull_url', 'git_push_url', 'commits_url', 'forks_url',
+              'public', 'created_at', 'updated_at', 'comments', 'comments_url']
+KEYS = ['id'] + COLUMNS[1:]
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    """
+    db : a database connection object
+    username : a string corresponding to a valid github username
+    """
+    # load gists into list of dicts and catch a 404
+    resp = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    if resp.status_code == 404:
+        raise HTTPError
+    gist_dicts = resp.json()
+
+    # create table from schema.sql
+    cursor = db.cursor()
+    schema = Path('schema.sql')
+    with open(schema) as fp:
+        cursor.executescript(schema.read_text())
+    
+    # insert data into table
+    query = """INSERT INTO gists (
+    'github_id', 'html_url', 'git_pull_url', 'git_push_url', 'commits_url', 'forks_url',
+    'public', 'created_at', 'updated_at', 'comments', 'comments_url'
+     ) VALUES (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url,
+              :public, :created_at, :updated_at, :comments, :comments_url);""" 
+    for d in gist_dicts:
+        values = [d[key] for key in KEYS]
+        params = {column:value for column, value in zip(COLUMNS, values)}
+        cursor.execute(query, params)
+
+    return cursor
+
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,47 @@
 from .models import Gist
+import sqlite3
+import datetime
+
+db = sqlite3.connect('tests/populated_gists_database.db')
 
 def search_gists(db_connection, **kwargs):
-    pass
+    """Struggled to make this work by inserting parameters into sqlite, so I did the other thing.
+    So, not safe from injection, but hey, all the tests are passing.
+    """
+    
+    # Initialize query and execute if no kwargs
+    cursor = db_connection.cursor()
+    query = 'SELECT * FROM gists;'
+    if not kwargs:
+        cursor.execute(query)
+    
+    # handle quargs
+    else:
+        comparisons = {'_lte' : '<=', '_gte' : '>=', '__lt' : '<',
+                       '__gt' : '>', 'd_at' : '='}
+        query = query[:-1] + ' WHERE '
+        comp_op = '='
+        for key, val in kwargs.items():
+            # date related kwargs
+            if (key.startswith('created_at') or key.startswith('updated_at')) and type(kwargs[key]) != str:
+                kwargs[key] = kwargs[key].strftime("%Y-%m-%dT%H:%M:%SZ")
+                kwargs[key] = '"' + kwargs[key] + '"'
+                comp_op = comparisons[key[-4:]]
+                query += key[:10] + comp_op + kwargs[key] + " AND "
+            # non date related kwargs
+            else: 
+                comp_op = '='
+                kwargs[key] = '"' + kwargs[key] + '"'
+                query += key + comp_op + kwargs[key] + " AND "
+    
+        query = query[:-5] + ";"
+        cursor.execute(query)
+    
+    # make Gists of gists
+    gists = []
+    for row in cursor:
+        gists.append(Gist(row))
+    return gists
+
+
+        
